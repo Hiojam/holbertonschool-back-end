d95b3f2b9035c463f95642ccc269602eedae3ea2
RoxxionYT <hiojam2016@gmail.com>
2023-12-20 14:57:08 -0300

    Fixed pep8 and data output task 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,25 +7,34 @@ Usage: python script_name.py employee_id
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
-    base_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    url = 'https://jsonplaceholder.typicode.com'
+    base_url = f'{url}/users/{employee_id}'
+    todos_url = f'{url}/todos?userId={employee_id}'
 
     try:
         employee_info = requests.get(base_url).json()
         todos = requests.get(todos_url).json()
 
         # Filter completed tasks
-        completed_tasks = [task['title'] for task in todos if task['completed']]
+        completed_tasks = [
+            task['title'] for task in todos if task['completed']
+            ]
         total_tasks = len(completed_tasks) + len(todos) - len(completed_tasks)
 
         # Display progress
-        print(f"Employee {employee_info['name']} is done with tasks ({len(completed_tasks)}/{total_tasks}):")
+        print(
+            f"Employee {employee_info['name']} is done with tasks"
+            f"({len(completed_tasks)}/{total_tasks}):"
+            )
+
         for task in completed_tasks:
-            print(f"\t{task}")
+            print(f"\t {task}")
 
     except requests.RequestException as e:
         print("Error fetching data:", e)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
