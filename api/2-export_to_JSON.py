5bc39d5bd7e80f75fa2ac039ae0644cc49d1993a
RoxxionYT <hiojam2016@gmail.com>
2023-12-20 15:34:58 -0300

    Task 2 done

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This script fetches exports information to Json
+Usage: python script_name.py employee_id
+"""
+
+import requests
+import json
+import sys
+
+
+def export_to_json(employee_id):
+    url = 'https://jsonplaceholder.typicode.com'
+    base_url = f'{url}/users/{employee_id}'
+    todos_url = f'{url}/todos?userId={employee_id}'
+
+    try:
+        employee_info = requests.get(base_url).json()
+        todos = requests.get(todos_url).json()
+
+        tasks = []
+        for task in todos:
+            task_data = {
+                "task": task['title'],
+                "completed": task['completed'],
+                "username": employee_info['username']
+            }
+            tasks.append(task_data)
+
+        output_data = {str(employee_id): tasks}
+
+        file_name = f"{employee_id}.json"
+        with open(file_name, 'w') as file:
+            json.dump(output_data, file, indent=2)
+
+        print(f"Data exported to {file_name}")
+
+    except requests.RequestException as e:
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py employee_id")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    export_to_json(employee_id)
